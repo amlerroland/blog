43cebf76dd1868130c7924e6fda9f4bf9f42af31
Molnár Dávid <molnar.david@swgy.hu>
2017-11-06 08:38:35 +0100

    Update 2017_10_26_162229_create_taggables_table.php
    Csak kicseréltem a morph mezőket, hogy lásd, hogy lehet így is hivatkozni, nem kell megadni külön a type és id variációkat.

--- database/migrations/2017_10_26_162229_create_taggables_table.php
+++ database/migrations/2017_10_26_162229_create_taggables_table.php
@@ -15,8 +15,7 @@ class CreateTaggablesTable extends Migration
     {
         Schema::create('taggables', function (Blueprint $table) {
             $table->integer('tag_id')->unsigned();
-            $table->integer('taggable_id');
-            $table->string('taggable_type');
+            $table->morphs('taggable');
 
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade')->onUpdate('cascade');
         });
